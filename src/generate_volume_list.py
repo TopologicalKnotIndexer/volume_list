a5506abf335ce6857286b7f310d82bef3fb63ffd
GGN_2015 <premierbob@qq.com>
2024-07-26 12:41:30 +0800

    update:name_to_pd_code

--- src/generate_volume_list.py
+++ src/generate_volume_list.py
@@ -7,7 +7,7 @@ except:
     tqdm = lambda x:x # 如果没有 tqdm 的进度条，那就凑合跑一下
 DIRNOW   = os.path.dirname(os.path.abspath(__file__))
 KNOTLIST = os.path.join(DIRNOW, "combined_knot_name.txt")
-INFOLIST = os.path.join(DIRNOW, "test_volume_info_list.txt")
+INFOLIST = os.path.join(DIRNOW, "volume_info_list.txt")
 assert os.path.isfile(KNOTLIST)
 
 from name_to_pd_code import name_to_pd_code
